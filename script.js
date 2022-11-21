46e7a8ad35f034366ba99496fc61d601e1411e37
graceywambui <grace.wambui@student.moringaschool.com>
2022-11-21 15:09:47 +0300

    changed font

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+const photos = [
+  {
+    src: './photo4.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: './photo3.jpeg',
+    width: 1,
+    height: 1
+  }
+];
+
+&lt;Gallery photos={photos} /&gt;;
+
+
+
+
+
+
+
 var form = document.getElementById('form')
 
     form.addEventListener('submit',function(event){
